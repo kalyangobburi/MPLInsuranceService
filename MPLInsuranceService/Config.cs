dfa6f815a5b1857662271058b849f2de8a88bd4a
V K CHAKRAVARTI GOBBURI <kalyan_gvk@hotmail.com>
2020-12-02 21:46:10 +0530

    MAPLE Test 2nd DEC 2020
    maple test files for interview

---
+++ MPLInsuranceService/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MPLInsuranceService
+{
+    public static class Config
+    {
+        public static string connectionString { get; set; }
+    }
+}
